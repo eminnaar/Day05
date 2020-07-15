f371cd848b402954c7f6ea0d228029e41e44da07
Eltonio Minnaar <eminnaar@c3r2s1.wethinkcode.co.za>
2020-07-15 16:40:06 +0200

    Completed ex05

---
+++ ex05/ft_strupcase.c
@@ -0,0 +1,13 @@
+char    *ft_strupcase(char *str)
+{
+    int x;
+    x = 0;
+
+    while (str[x] != '\0')
+    {
+        if (str[x] >= 'a' && str[x] <= 'z')
+        str[x] -= 32;
+        x++;
+    }
+    return (str);
+}
